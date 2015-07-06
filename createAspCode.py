b1ef85b4659e1e9be5b2fa483b95c14cc4d7bb88
Julius Gonsior <julius@gaenseri.ch>
2015-07-06 10:04:52 +0200

    …

---
+++ createAspCode.py
@@ -0,0 +1,26 @@
+asp = "%trafficlightgreen(light l, timestamp t)\n\n"
+
+#in seconds
+timeDuration = 30
+startTime = (1/timeDuration) * 6*60*60
+endTime = (1/timeDuration) *21*60*60
+
+
+for l in range(1,9):
+  asp +=  "trafficlightgreen({},{}..{}).".format(l,startTime, endTime) + "\n"
+
+asp += "\n"
+
+
+#let there be time
+asp += "%follow(time t, time t+1)\n"
+asp += "follow({:.0f}..{:.0f}, {:.0f}..{:.0f}).".format(startTime, endTime, startTime+1, endTime+1) + "\n"
+
+
+
+fh = open("asp.lp", "w")
+fh.write(asp)
+fh.close()
+
+print(asp)
+
