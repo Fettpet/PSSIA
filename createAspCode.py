bc82f805ff75f1e4056794dc9adaaafe70dbfd28
IlT <seb.hahn1991@googlemail.com>
2015-07-06 16:46:52 +0200

    Python angepasst

--- createAspCode.py
+++ createAspCode.py
@@ -1,8 +1,8 @@
 # in seconds
 timeDuration = 30 
-startTime = (1 / timeDuration) * 6 * 60 * 60
-endTime = (1 / timeDuration) * 21 * 60 * 60
-
+startTime = 1 #(1 / timeDuration) * 6 * 60 * 60
+endTime = 10 #(1 / timeDuration) * 21 * 60 * 60
+maxVariablesFalse = 120/timeDuration
 
 
 
@@ -36,8 +36,7 @@ asp = """
 
 
 % facts
-trafficLightCars(1..8).
-trafficLightPed(9..16).
+trafficLight(1..16).
 time({:.0f}..{:.0f}).
 
 
@@ -51,17 +50,22 @@ carsPerScond(7,0).
 carsPerScond(8,0).
 
 
+% generate
+{{green(L, T) :trafficLight(L), time(T)}}.
+{{conflict(lo, ls) : trafficLight(lo), trafficLight(ls)}}
 
 %let there be time
 follow(T1, T2) :- time(T1), time(T2), T1+1=T2.
 
 
 % generate
-green(L, T) :- trafficLightCars(L), time(T).
-green(L, T) :- trafficLightPed(L), time(T).
-
-
-% define
+{{green(L, T) :trafficLight(L), time(T)}}.
+{{green(L, T) :trafficLight(L), time(T)}}.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%						Constraint: Minimal Green phase (a)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Error since it use seconds insteand of half minute
+% Error not insteand of -
 % if ped. traffic light turns green, it stays green for X seconds
 20 {{green(L,T..T+20)}} 20 :- -green(L,T-1), green(L,T), L=9.
  7 {{green(L,T..T+ 7)}}  7 :- -green(L,T-1), green(L,T), L=10.
@@ -74,22 +78,36 @@ green(L, T) :- trafficLightPed(L), time(T).
 
 
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%						Constraint: No accidents (c)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+:- green(LO, T), green(LT, T), time(T), trafficlight(LO), trafficlight(LT), conflicts(LO, LT).
+:- green(LT, T), green(LO, T), time(T), trafficlight(LO), trafficlight(LT), conflicts(LO, LT).
+conflicts(1, 3) , trafficlight(1), trafficlight(3).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%						Constraint: Maximal red phase (d) Unbekannt
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %CONTAINS SYNTAX ERROR:
 % if traffic light turns red, it is not allowed to stay red for more than 120 seconds
 %lengthGreen(L,T,X=1) :- -green(L,T-1), green(L,T), green(L,T+1).
 %lengthGreen(L,T,X+1) :- green(L,T+1), lengthGreen(L,T,X).
 %:- lengthGreen(L,T,X), X > 120.
-
+:- {:.0f}{{ not green(L, T..T+{:.0f})}}{:.0f}, trafficLight(L), time(T).
+% if traffic light turns red, it is not allowed to stay red for more than 120 seconds
+%lengthGreen(L,T,X=1) :- -green(L,T-1), green(L,T), green(L,T+1).
+%lengthGreen(L,T,X+1) :- green(L,T+1), lengthGreen(L,T,X).%:- lengthGreen(L,T,X), X > 120.
 
 
 %constraint c, between switching of green phases consider 3 seconds of red phase for all directions
--green(1..8,T..T+3) :- green(L,T-1), - green(L,T).
+%-green(1..8,T..T+3) :- green(L,T-1), - green(L,T).
 
 
 
 
 
-""".format(startTime, endTime)
+""".format(startTime, endTime,  maxVariablesFalse,  maxVariablesFalse-1,  maxVariablesFalse)
 
 
 
