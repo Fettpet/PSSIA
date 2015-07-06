e1131b2c21997dd3fe49d0e58b26c6d917e41e90
Julius Gonsior <julius@gaenseri.ch>
2015-07-06 10:23:32 +0200

    fixes floating point error in python script

--- createAspCode.py
+++ createAspCode.py
@@ -1,21 +1,22 @@
 asp = "%trafficlightgreen(light l, timestamp t)\n\n"
 
-#in seconds
+# in seconds
 timeDuration = 30
-startTime = (1/timeDuration) * 6*60*60
-endTime = (1/timeDuration) *21*60*60
+startTime = (1 / timeDuration) * 6 * 60 * 60
+endTime = (1 / timeDuration) * 21 * 60 * 60
 
 
-for l in range(1,9):
-  asp +=  "trafficlightgreen({},{}..{}).".format(l,startTime, endTime) + "\n"
+for l in range(1, 9):
+    asp += "trafficlightgreen({:.0f},{:.0f}..{:.0f}).".format(l,
+                                                              startTime, endTime) + "\n"
 
 asp += "\n"
 
 
-#let there be time
+# let there be time
 asp += "%follow(time t, time t+1)\n"
-asp += "follow({:.0f}..{:.0f}, {:.0f}..{:.0f}).".format(startTime, endTime, startTime+1, endTime+1) + "\n"
-
+asp += "follow({:.0f}..{:.0f}, {:.0f}..{:.0f}).".format(startTime,
+                                                        endTime, startTime + 1, endTime + 1) + "\n"
 
 
 fh = open("asp.lp", "w")
@@ -23,4 +24,3 @@ fh.write(asp)
 fh.close()
 
 print(asp)
-
